534db613af63fc581387ab20ae7b6a6b9c3d8fa1
keith <keithneo00@gmail.com>
2021-07-21 23:11:23 +0800

    commit

--- Q1Output.py
+++ Q1Output.py
@@ -2,6 +2,24 @@
 students = {}
 counter = 0
 
+# Gpa = (gpa of mod * credit score (total grade point)) / total credit score
+
+#function to calculate student gpa 
+def calGPA():
+    totalcredit = 0
+    totalgradepoint = 0
+    moduleNum = int(input("How many modules did the student take: "))
+    print("")
+    for x in range(moduleNum):
+        gpaformod = float(input("Enter gpa for module %d: " % x))
+        creditscore = int(input("Enter credit score for module %d: " % x))
+        print("")
+        totalcredit += creditscore
+        totalgradepoint += (gpaformod * creditscore)
+    gpa = totalgradepoint / totalcredit
+    return gpa
+
+
 #add student function 
 def addStudent():
     global students
@@ -9,10 +27,15 @@ def addStudent():
     print("Add new Student to dictionary")
     studentName = input("Enter Student name: ")
     studentCourse = input("Enter Student course: ")
-    studentGpa = float(input("Enter Student GPA: "))
-
+    ask = input("Do you know the student's GPA? [y/n]: ")
     name = "student" + str(counter)
-    students[name] = {"name":studentName, "Course":studentCourse, "GPA":studentGpa}
+    if ask.lower() == "n":
+        gpa = format(calGPA(), '.2f')
+        students[name] = {"name":studentName, "Course":studentCourse, "GPA":gpa}
+    else:
+        studentGpa = float(input("Enter Student GPA: "))
+        students[name] = {"name":studentName, "Course":studentCourse, "GPA":studentGpa}
+    
     counter += 1
     
 
